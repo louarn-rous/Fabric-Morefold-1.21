bab5924e487e9aa7e29dc125930dbb39ff2345b2
louarn-rous <afeg64@gmail.com>
2025-02-13 21:10:56 +1100

    Initial commit

---
+++ src/main/java/net/niev/morefold/Morefold.java
@@ -0,0 +1,16 @@
+package net.niev.morefold;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Morefold implements ModInitializer {
+	public static final String MOD_ID = "morefold";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/niev/morefold/MorefoldClient.java
@@ -0,0 +1,10 @@
+package net.niev.morefold;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MorefoldClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
